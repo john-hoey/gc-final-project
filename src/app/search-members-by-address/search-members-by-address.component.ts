aac4779d5e7302b803e2cdb086b687cf9439dcf4
Dominick <dominickv1998@gmail.com>
2021-03-17 11:16:04 -0400

    added search by address to google civic

---
+++ src/app/search-members-by-address/search-members-by-address.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-members-by-address',
+  templateUrl: './search-members-by-address.component.html',
+  styleUrls: ['./search-members-by-address.component.css'],
+})
+export class SearchMembersByAddressComponent implements OnInit {
+  @Output() addressSearchEvent = new EventEmitter<string>();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  emitAddress = (form: NgForm) => {
+    console.log(form);
+    this.addressSearchEvent.emit(form.form.value.address);
+    form.reset();
+  };
+}
